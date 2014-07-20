4afbd3a1047f6e4042c007768149461b727c984d
Graham Gilbert <graham@grahamgilbert.com>
2014-07-20 11:37:16 +0100

    Ensure and absent on the define

--- manifests/every.pp
+++ manifests/every.pp
@@ -1,14 +1,27 @@
 define scriptrunner::every(
     $script,
-    $priority = $scriptrunner::priority
+    $priority = $scriptrunner::priority,
+    $ensure = 'present'
 ){
     require scriptrunner::setup
 
-    file {"/Library/Management/scriptRunner/every/${priority}-${title}":
-        source => $script,
-        owner  => 0,
-        group  => 0,
-        mode   => '0755',
+    if $ensure != 'present' and $ensure !='absent'{
+        fail('Invalid value for ensure')
+    }
+
+    if $ensure = 'present'{
+        file {"/Library/Management/scriptRunner/every/${priority}-${title}":
+            source => $script,
+            owner  => 0,
+            group  => 0,
+            mode   => '0755',
+        }
+    }
+
+    if $ensure = 'absent' {
+        file {"/Library/Management/scriptRunner/every/${priority}-${title}":
+            ensure => absent,
+        }
     }
 
 }
--- manifests/once.pp
+++ manifests/once.pp
@@ -1,14 +1,26 @@
 define scriptrunner::once(
     $script,
-    $priority = $scriptrunner::priority
+    $priority = $scriptrunner::priority,
+    $ensure = 'present'
 ){
     require scriptrunner::setup
 
-    file {"/Library/Management/scriptRunner/once/${priority}-${title}":
-        source => $script,
-        owner  => 0,
-        group  => 0,
-        mode   => '0755',
+    if $ensure != 'present' and $ensure !='absent'{
+        fail('Invalid value for ensure')
+    }
+    
+    if $ensure = 'present'{
+        file {"/Library/Management/scriptRunner/once/${priority}-${title}":
+            source => $script,
+            owner  => 0,
+            group  => 0,
+            mode   => '0755',
+        }
     }
 
+    if $ensure = 'absent' {
+        file {"/Library/Management/scriptRunner/once/${priority}-${title}":
+            ensure => absent,
+        }
+    }
 }
